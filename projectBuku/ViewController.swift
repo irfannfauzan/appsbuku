b5afb5e54c7b615aa40d097d4e11396d1425d4cc
Irfan Fauzan <irfannfauzan@irfans-macbook-air.local>
2020-11-20 22:13:14 +0700

    Initial Commit

---
+++ projectBuku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectBuku
+//
+//  Created by Irfan Fauzan R on 20/11/20.
+//  Copyright © 2020 Irfan Fauzan R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
